583592798255bdaa4f5d3d3720f079d7cb0d8510
zouyingjie <yingjiezou1@gmail.com>
2018-04-03 11:14:56 +0800

    code review

--- lifefitness/views.py
+++ lifefitness/views.py
@@ -8,4 +8,4 @@ from django.views import View
 class LifeFitnessCallBackView(View):
 
     def get(self, request, *args, **kwargs):
-        return HttpResponse()
+        return HttpResponse("Hello World")
